09cd74db6f3aa92052e536e5d85629b024e2b7d7
tangjingchun <tangjingchun@bytedance.com>
2021-03-14 16:47:07 +0800

    fix: bug

--- image-compress/server.js
+++ image-compress/server.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const app = express()
 const path = require('path')
 const upload = require('./upload.js');
+const https = require('https');
+const fs = require('fs');
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,4 +13,10 @@ app.use(bodyParser.json())
 // 配置静态资源目录 整一个文件夹 通过域名能访问
 app.use(express.static(path.normalize(path.join(__dirname, "../static"))))
 app.use('/upload', upload)
-app.listen(10, () => console.log("图片上传服务器开启，端口: 10"))
+
+https.createServer({
+  key: fs.readFileSync("../ssl/Nginx/2_www.dododawn.com.key"),
+  cert: fs.readFileSync("../ssl/Nginx/1_www.dododawn.com_bundle.crt")
+}, app).listen(10, () => {
+  console.log("图片上传服务器开启，端口: 10")
+});
